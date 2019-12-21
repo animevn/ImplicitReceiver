6a925da53292a1b4eb308823c12f8e3c556c91bc
animevn <haanhth21@gmail.com>
2019-12-21 15:35:08 +0700

    decode and reduce size of bitmap

--- app/src/main/java/com/haanhgs/app/implicitreceiverdemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/implicitreceiverdemo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.haanhgs.app.implicitreceiverdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvMessage)
+    TextView tvMessage;
+    @BindView(R.id.ivImage)
+    ImageView ivImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
---
+++ app/src/main/java/com/haanhgs/app/implicitreceiverdemo/Repo.java
@@ -0,0 +1,47 @@
+package com.haanhgs.app.implicitreceiverdemo;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.ImageView;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class Repo {
+
+    private static Bitmap decodeUri(Context context, Uri uri, int size)throws FileNotFoundException{
+        InputStream inputStream = context.getContentResolver().openInputStream(uri);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(inputStream, null, options);
+        int widthTemp = options.outWidth;
+        int hightTemp = options.outHeight;
+        int scale = 1;
+
+        while (widthTemp/2 >= size && hightTemp/2 >= size){
+            widthTemp /= 2;
+            hightTemp /= 2;
+            scale *= 2;
+        }
+
+        BitmapFactory.Options newOptions = new BitmapFactory.Options();
+        options.inSampleSize = scale;
+        return  BitmapFactory.decodeStream(inputStream, null, newOptions);
+    }
+
+    public static void loadImageFromUri(Context context, Uri uri, ImageView imageView, int size){
+        if (uri != null){
+            try{
+                Bitmap bitmap = decodeUri(context, uri, size);
+                if (bitmap != null){
+                    imageView.setImageBitmap(bitmap);
+                }
+            }catch (FileNotFoundException e){
+                Log.e("E.Repo", "file not found");
+            }
+        }
+    }
+}
