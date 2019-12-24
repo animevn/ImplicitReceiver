5676db59f33f4988b28920868fd58a0c32b1021a
animevn <haanhth21@gmail.com>
2019-12-24 11:33:30 +0700

    add permission ok

--- app/src/main/java/com/haanhgs/app/implicitreceiverdemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/implicitreceiverdemo/MainActivity.java
@@ -1,20 +1,26 @@
 package com.haanhgs.app.implicitreceiverdemo;
 
-import android.content.Context;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import java.io.FileNotFoundException;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST = 1979;
     @BindView(R.id.tvMessage)
     TextView tvMessage;
     @BindView(R.id.ivImage)
@@ -23,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private Intent intent;
     private String action;
     private String type;
+    private Uri uri;
 
 
     private void prepareIntent(){
@@ -39,15 +46,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void loadImageFromUri(Context context, Uri uri){
+    private void loadImageFromUri(){
         if (uri != null){
             try{
-                Bitmap bitmap = Repo.decodeUri(context, uri, 300);
+                Bitmap bitmap = Repo.decodeUri(this, uri, 300);
                 if (bitmap != null){
                     ivImage.setImageBitmap(bitmap);
                 }
             }catch (FileNotFoundException e){
                 Log.e("E.Repo", "file not found");
+            }
+        }
+    }
+
+    //remember to add permission to manifest
+    private void checkReadPermission(){
+        int p = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (p != PackageManager.PERMISSION_GRANTED){
+            String[] permissions = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST);
+
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+//                Toast.makeText(this, "need this to read image on storage",
+//                        Toast.LENGTH_SHORT).show();
+//            } else {
+//                ActivityCompat.requestPermissions(
+//                        this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
+//            }
+        } else {
+            loadImageFromUri();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                loadImageFromUri();
+            } else {
+                Toast.makeText(this, "request denied", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -63,8 +103,8 @@ public class MainActivity extends AppCompatActivity {
 
             //receive image ok now
             if (type.startsWith("image/")){
-                Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
-                loadImageFromUri(this, uri);
+                uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                checkReadPermission();
             }
         }
     }
